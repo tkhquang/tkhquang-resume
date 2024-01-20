b524302f3ce5f1574460638d2431c8cd4ae330ef
Quang Trinh <khacquang.trinh@gmail.com>
2024-01-20 21:59:00 +0700

    Fix build error

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        loose: true,
+        targets: {
+          node: "14",
+          browsers: "last 2 versions",
+        },
+      },
+    ],
+    ["@babel/preset-react", { runtime: "automatic" }],
+  ],
+  plugins: [
+    ["@babel/plugin-transform-runtime", { version: "^7.19.6" }],
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    "@babel/plugin-proposal-optional-chaining",
+  ],
+};
